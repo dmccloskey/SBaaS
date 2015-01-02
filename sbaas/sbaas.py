0a9c8e4af9e5079cf6d8d83dc852e8b3429b6ec2
dmccloskey <dmccloskey87@gmail.com>
2015-01-02 14:16:08 -0800

    remove ignored files

--- sbaas/sbaas.py
+++
@@ -1,9 +0,0 @@
-#Analysis tests:
-from tests import analysis_ale, analysis_physiology, analysis_resequencing
-analysis_ale.run_all_tests();
-analysis_physiology.run_all_tests();
-analysis_resequencing.run_all_tests();
-
-#Visualization tests:
-from visualization.server import run
-run();
